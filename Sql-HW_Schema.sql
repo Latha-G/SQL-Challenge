9645cf8a2ea2606742f9275b5c4f57cfbedc3ac7
Latha-G <60633643+Latha-G@users.noreply.github.com>
2020-04-19 23:34:52 -0700

    SQL HW

--- Sql-HW_Schema.sql
+++ Sql-HW_Schema.sql
@@ -17,45 +17,45 @@ create table employees(
 );
 
 create table departments(
-	dept_no 	VARCHAR		not null,
-	dept_name 	VARCHAR		not null,
+	dept_no     VARCHAR   not null,
+	dept_name   VARCHAR   not null,
 	primary key (dept_no)
 );
 
 create table dept_manager(
-	dept_no		VARCHAR not null,
-	emp_no 		INT 	not null,
-	from_date 	DATE	not null,
-	to_date 	DATE	not null,
+	dept_no     VARCHAR   not null,
+	emp_no      INT       not null,
+	from_date   DATE      not null,
+	to_date     DATE      not null,
 	PRIMARY KEY (dept_no,emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 create table dept_emp(
-	emp_no 		INT 	not null ,
-	dept_no		VARCHAR not null,
-	from_date 	DATE	not null,
-	to_date 	DATE	not null,
+	emp_no      INT      not null ,
+	dept_no     VARCHAR  not null,
+	from_date   DATE     not null,
+	to_date     DATE     not null,
 	PRIMARY KEY (emp_no,dept_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 create table titles(
-	emp_no      INT     not null,
-	title		VARCHAR not null,
-	from_date	DATE    not null,
-	to_date		DATE,
+	emp_no      INT      not null,
+	title       VARCHAR  not null,
+	from_date   DATE     not null,
+	to_date     DATE,
 	PRIMARY KEY (emp_no,title,from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 create table salaries(
-	emp_no		INT		not null,
-	salary		INT		not null,
-	from_date	DATE	not null,
-	to_date		DATE	not null,
+	emp_no      INT     not null,
+	salary      INT     not null,
+	from_date   DATE    not null,
+	to_date     DATE    not null,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
